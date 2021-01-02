91b86287bee792d5dbb3827c2f92dba9914f26f1
Raj <raj.rajeev2244@gmail.com>
2021-01-02 12:45:55 +0530

    Intial Commit

---
+++ index.js
@@ -0,0 +1,60 @@
+console.log('Welcome To Java Script')
+console.log(1000+100+3+24542+525252+25252+252525)
+
+
+console.log('Data Types')
+
+var esalary
+
+var employee={
+    eid:1,
+    ename:'Prudhvi',
+    ecity:'Bangalore'
+}
+
+console.log(typeof(esalary))
+
+var estreet=null
+
+console.log(typeof(estreet))
+
+
+console.log(typeof(employee))
+console.log(employee)
+
+console.log('Employee ID :',employee.eid)
+console.log('Employee Name :',employee.ename)
+console.log('Employee City :',employee.ecity)
+console.log('Employee Email :',employee.email)
+
+
+/*
+var a=10
+var b=900.555
+var married=true
+var ename='Raj Prudhvi'
+var ecity="Bangalore"
+
+var colors=['Red','Green','Yellow','Pink',1,2,3,54,true]
+
+console.log(typeof(a))
+console.log(typeof(b))
+console.log(typeof(married))
+console.log(typeof(ename))
+console.log(typeof(ecity))
+console.log(typeof(colors))
+console.log(Array.isArray(colors))
+console.log(Array.isArray(ecity))
+
+console.log(a)
+console.log(b)
+console.log(married)
+console.log(ename)
+console.log(ecity)
+console.log(colors)
+
+*/
+
+
+
+
---
+++ object.js
@@ -0,0 +1,78 @@
+/*
+var employee={
+    eid:1,
+    ename:'Prudhvi',
+    ecity:'Bangalore'
+}
+
+
+var employees=[
+    {eid:1,ename:'Prudhvi',ecity:'Bangalore'},
+    {eid:2,ename:'Prakash',ecity:'Bangalore'},
+    {eid:3,ename:'Pavan',ecity:'Bangalore'},
+    {eid:4,ename:'Prasad',ecity:'Bangalore'},
+    {eid:5,ename:'Praveen',ecity:'Bangalore'}
+]
+
+console.log(typeof(employee))
+console.log(employee)
+
+console.log('Employee ID :',employee.eid)
+console.log('Employee Name:',employee.ename)
+console.log('Employee City :',employee.ecity)
+
+for(employee1 in employee)
+{
+    console.log(employee[employee1])
+}
+
+
+console.log('*********************')
+
+employees.forEach(function(employee)
+{
+    console.log('Employee ID :',employee.eid)
+    console.log('Employee Name :',employee.ename)
+    console.log('Employee City :',employee.ecity)
+    console.log('****************************************')
+})
+
+console.log('---------------------------------------------------------------')
+employees.map(function(employee)
+{
+    console.log('Employee ID :',employee.eid)
+    console.log('Employee Name :',employee.ename.toUpperCase())
+    console.log('Employee City :',employee.ecity)
+    console.log('****************************************')
+})
+
+console.log('-=-=-=-=-=-=-=-=-=-=-=-=-=-==-==-=-=-=-=-=--=')
+
+for(employee of employees)
+{
+    console.log('Employee ID :',employee.eid)
+    console.log('Employee Name :',employee.ename.toUpperCase())
+    console.log('Employee City :',employee.ecity)
+    console.log('****************************************')
+}
+
+console.log('<><><><><><><><><><><><><><><><><><><><><><><><><><>')
+
+for(employee in employees)
+{
+    console.log('Employee ID :',employees.employee[0])
+    console.log('Employee Name :',employees[employee[1]])
+    console.log('Employee City :',employees[employee[2]])
+    console.log('****************************************')
+}
+
+
+*/
+
+
+
+
+
+
+
+
